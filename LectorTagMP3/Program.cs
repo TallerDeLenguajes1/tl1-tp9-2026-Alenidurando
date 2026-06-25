88b7970e8f89f3d31988f20e05438870e6e1883a
Alejandro Durando <ale.durando@outlook.com>
2026-06-25 19:56:26 -0300

    punto 2 hecho

---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,23 @@
+﻿using tag;
+using System.IO;
+using System.Text;
+string path="prueba.mp3";
+
+Id3v1Tag audio = new Id3v1Tag();
+FileStream fs = new FileStream(path,FileMode.Open,FileAccess.Read);
+
+fs.Seek(-128, SeekOrigin.End); //leer desde 128bytes antes del final del archivo.
+
+BinaryReader lector = new BinaryReader(fs);
+
+byte[] datos = lector.ReadBytes(128);
+
+audio.Titulo = Encoding.GetEncoding("latin1").GetString(datos, 3, 30).TrimEnd('\0', ' ');
+audio.Album= Encoding.GetEncoding("latin1").GetString(datos, 63, 30).TrimEnd('\0', ' ');
+audio.Anio= Encoding.GetEncoding("latin1").GetString(datos, 93, 30).TrimEnd('\0', ' ');
+audio.Artista= Encoding.GetEncoding("latin1").GetString(datos, 33, 30).TrimEnd('\0', ' ');
+
+
+System.Console.WriteLine("Datos del mp3: \nTitulo: "+audio.Titulo+"\nAño: "+audio.Anio+"\nArtista: "+audio.Artista+"\nAlbum: "+audio.Album);
+
+
---
+++ LectorTagMP3/tag.cs
@@ -0,0 +1,10 @@
+namespace tag;
+
+public class Id3v1Tag
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public string Album { get; set; }
+    public string Anio { get; set; }
+
+}
